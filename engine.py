f2d2e0013f2c92f7de8512bbe3273cf47e7ac738
Umid Ganbarov <uganbarov23846@ada.edu.az>
2026-06-17 05:38:32 +0400

    Finalizing simulation, Add player average displacement and update options

--- engine.py
+++ engine.py
@@ -17,12 +17,16 @@ def get_score(x,y):
     if r<=2:return 5
     if r<=3:return 1
     return 0
-
+def get_player_average_displacement(player):
+    global df
+    player-=1
+    x=df.iloc[player::3][0].mean()
+    y=df.iloc[player::3][1].mean()
+    return (x,y)
 def get_player_score(player):
     global df
     player-=1 #easier for index
     player_score=0
-    
     for ind in range(player, len(df),3):
         row=df.iloc[ind]
         x=row[0]
@@ -41,8 +45,8 @@ def option1():
             plot1_123(num)
         elif num ==4:
             plot1_4()
-        
         elif num==6:break
+        elif num==5:plot1_5()
 
 def plot1_123(player):
     global df
@@ -102,8 +106,65 @@ def plot1_4():
     plt.title("Dart Hits for All Players",c="red")
     plt.show()
 
+def plot1_5():
+    global df
+    xs=np.array(["Player 1","Player 2","Player 3"])
+    y0=[0,0,0]
+    y1=[0,0,0]
+    y5=[0,0,0]
+    y10=[0,0,0]
+    
+    for ind in range( len(df)):
+        player=ind%3+1
+        score=get_score(df.iloc[ind][0],df.iloc[ind][1])
+        if score==0:y0[player-1]+=1
+        if score==1:y1[player-1]+=1
+        if score==5:y5[player-1]+=1
+        if score==10:y10[player-1]+=1
+    y0=np.array(y0)
+    y1=np.array(y1)
+    y5=np.array(y5)
+    y10=np.array(y10)
 
+    plt.bar(xs,y0,label="0 Points",color="purple")
+    plt.bar(xs,y1,label="1 Points",color="green",bottom=y0)
+    plt.bar(xs,y5,label="5 Points",color="blue",bottom=y0+y1)
+    plt.bar(xs,y10,label="10 Points",color="red",bottom=y0+y1+y5)
+    plt.title("Frequency of Hits")
+    plt.legend(loc="best")
+    plt.show()
 
+def option2():
+    global df
+    p1=get_player_score(1)
+    p2=get_player_score(2)
+    p3=get_player_score(3)
+    scrs=[p1,p2,p3]
+    p1a=get_player_average_displacement(1)
+    p2a=get_player_average_displacement(2)
+    p3a=get_player_average_displacement(3)
+    print(f"""--- OVERALL SCORES ---
+Player 1: {p1}
+Player 2: {p2}
+Player 3: {p3}\n &""")
+    time.sleep(1.5)
+    print(f"""=== AVERAGE DISPLACEMENTS (x,y) ===
+Player 1: ({p1a[0]:.4f},{p1a[1]:.4f})
+Player 2: ({p2a[0]:.4f},{p2a[1]:.4f})
+Player 3: ({p3a[0]:.4f},{p3a[1]:.4f})
+""")
+    print("Considering All stats\nThe Winner is")
+    time.sleep(.5)
+    print("The Winner is.")
+    time.sleep(.5)
+    print("The Winner is..")
+    time.sleep(.5)
+    print("The Winner is...")
+    time.sleep(.5)
+    print("The Winner is....")
+    time.sleep(.5)
+    print(f"The Winner is Player {scrs.index(max(scrs))+1}\nCongratulations Player {scrs.index(max(scrs))+1}, nice hits.\n")
+    
 def main_menu():
     while True:
         print("""Choose an option:\n1. Plots\n2. Analyse Data\n3. Exit""")
@@ -116,29 +177,12 @@ def main_menu():
             exit()
         elif num==1:
             option1()
-
-
-
-    
-def main():
-    global df
-    df=pd.read_csv("data.csv",header=None)
-    main_menu()
-
-
-
-
-
-
-
-
-
-
-
-
+        elif num==2:
+            option2()
 
 
 
 if __name__=="__main__":
-    main()
-    print("\nDONE\n")
+    global df
+    df=pd.read_csv("data.csv",header=None)
+    main_menu()
